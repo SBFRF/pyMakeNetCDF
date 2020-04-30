d32ba73947816c4d767a550dd3f286c069de8e27
Spicer Bak <spicer.bak.frf@gmail.com>
2020-04-30 11:06:07 -0400

    updated working file to have appropriate files

--- scratch.py
+++ scratch.py
@@ -1,6 +1,6 @@
 import py2netCDF as p2nc
 
-inputFile = "/home/spike/cmtb/data/waveModels/ww3/base/2018-03-02T000000Z/2018-03-02T000000Z.nc"
+inputFile = "/home/spike/cmtb/data/waveModels/ww3/base/2018-03-02T000000Z/ww3.201803.nc"
 varYaml = "/home/spike/repos/makeNetCDF/Field_var.yml"
 globalYaml  = "/home/spike/repos/makeNetCDF/field_globalmeta.yml"
 
