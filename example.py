aa841eeef97756ae312ebba4878fba525913fade
Spicer Bak <spicer.bak.frf@gmail.com>
2020-05-08 09:25:31 -0400

    cleaned up example

--- example.py
+++ example.py
@@ -2,31 +2,8 @@ import py2netCDF as p2nc
 import pickle, gridded
 import netCDF4 as nc
 
-# inputFile = "/home/spike/cmtb/data/waveModels/ww3/base/2018-03-02T000000Z/ww3.201803.nc"
 varYaml = "/home/spike/repos/makeNetCDF/Station_Directional_Wave_var.yml"
 globalYaml = "/home/spike/repos/makeNetCDF/Station_globalmeta.yml"
-#
-# with open('mesh.pickle', 'rb') as fid:
-#     meshData = pickle.load(fid)
-#
-# p2nc.ncFile2ncFile(inputFile=inputFile, globalYaml=globalYaml, varYaml=varYaml, meshData=meshData)
-# # Store data (serialize)
-#
-# ugridFile = "/home/spike/repos/makeNetCDF/ww3.201803_spec.ncfile"
-# ugf = gridded.Dataset(ugridFile)
-#
-# nc = nc.Dataset(ugridFile)
-#
-# # need to convert to zero-indexing
-# nodes = nc.variables['nodes'][:] - 1
-# faces = nc.variables['E3T'][:, :3] - 1
-#
-# # make the grid
-# # gridded.grids.Grid_U
-# grid = gridded.grids.Grid_U(nodes=meshData['points'],
-#                             faces=faces,
-#                             )
-
 ######### load process ww3 spec files #######3
 import sys
 sys.path.append('/home/spike/repos')
@@ -58,4 +35,4 @@ for ss, station in enumerate(nc.chartostring(ncfile['station_name'][:])):
            'directionalWaveEnergyDensity': dWEDout[:,ss],
            'station_name': station
     }
-    p2nc.makenc_generic('test.nc', globalYaml, varYaml, out)
+    p2nc.makenc_generic('test{}.nc'.format(ss), globalYaml, varYaml, out)
